0f9cfed2b6641cf3ed406f5b35f44b9a3ad50a20
lenovalsha <lenovalsha@gmail.com>
2023-04-24 11:54:19 -0600

    ready

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -2,24 +2,21 @@ import React from "react";
 
 const ContactMe = () => {
   return (
-    <div class="container">
+    <div class="container contact">
       <div class="row">
-        <div
-          class="col-lg-12"
-          style="text-align: center; margin-top: 100px; margin-bottom: 100px"
-        >
+        <div class="col-lg-12">
           <p>
             For business inquiries, questions, and requests, the best places to
             reach me are at my email:
-            <span style="font-weight: 600">
+            <span>
               <a href="mailto:zackaryguckert@gmail.com">
-                zackaryguckert@gmail.com
+                {" zackaryguckert@gmail.com "}
               </a>
             </span>
             or
-            <span style="font-weight: 600">
+            <span>
               <a href="https://www.linkedin.com/in/zackary-guckert/">
-                LinkedIn Messaging
+                {" LinkedIn Messaging"}
               </a>
             </span>
           </p>
