c3b5abf6cfda7361351dfe13f8405067b5cb14a1
lenovalsha <lenovalsha@gmail.com>
2023-05-07 16:18:46 -0600

    deploying

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import "./index.css";
 import App from "./App";
 import Footer from "./layouts/Footer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// const footer = ReactDOM.createRoot(document.getElementById("footer"));
+const footer = ReactDOM.createRoot(document.getElementById("footer"));
 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-// footer.render(
-//   <React.StrictMode>
-//     <Footer />
-//   </React.StrictMode>
-// );
+footer.render(
+  <React.StrictMode>
+    <Footer />
+  </React.StrictMode>
+);
